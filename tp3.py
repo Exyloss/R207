00de301903382459e255df39925e2c579bc7390a
Antonin PONS <antoninpons@protonmail.com>
2023-02-23 11:52:17 +0100

    commit

--- tp3.py
+++ tp3.py
@@ -32,8 +32,37 @@ def bdd2json(req: str, file: str) -> int:
     f.write(string)
     return 1
 
-def json2bdd(file: str) -> int:
-    data = json.loads(open(file, "r").read())
+def json2bdd(file: str, table: str, bdd: str, types: str) -> int:
+    """
+    Format JSON d'entrée :
+    [
+        {clef: attribut, ...},
+        ...
+    ]
+    """
+    data    = json.loads(open(file, "r").read())
+    con_fun = sqlite3.connect(bdd)
+    cur_fun = con_fun.cursor()
+    attributes = [i for i in (data[0]).keys()]
+    for i in range(len(types)):
+        attributes[i] += " "+types[i]+" NOT NULL"
+    attr_str = ", ".join(attributes)
+
+    cur_fun.execute("CREATE TABLE "+table+"("+attr_str+");")
+
+    for i in data:
+        insert_values = []
+        for j in i.values():
+            if isinstance(j, int):
+                elt = str(j)
+            else:
+                elt = '"'+str(j)+'"'
+            insert_values.append(elt)
+
+        cur_fun.execute("INSERT INTO "+table+" VALUES ("+",".join(insert_values)+");")
+    con_fun.commit()
+    return 1
+
 
 if __name__ == "__main__":
     import sqlite3
@@ -44,6 +73,6 @@ if __name__ == "__main__":
 
     html_print("SELECT * FROM Machine;")
     bdd2json("SELECT * FROM Machine;", "file.json")
-    json2bdd("file.json")
+    json2bdd("test.json", "table1", "test.sqlite", ["INTEGER", "TEXT"])
 
     con.close()
